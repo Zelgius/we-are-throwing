e09763cb164b10f2bc48ec9ea17b67629e9280b9
Mason Egger <mason@masonegger.com>
2018-12-05 18:17:06 -0600

    removign useless craps

--- build/lib/we_are_throwing/constants.py
+++
@@ -1,5 +0,0 @@
-OW_HEROES = ['ana', 'bastion', 'brigitte', 'doomfist', 'dva', 'genji',
-             'hanzo', 'junkrat', 'lucio', 'mccree', 'mei', 'mercy',
-             'moira', 'orisa', 'pharah', 'reaper', 'reinhardt', 'roadhog',
-             'soldier76', 'sombra', 'symmetra', 'tracer', 'torbjorn',
-             'winston', 'wrecking_ball', 'zarya', 'zenyatta']
